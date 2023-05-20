74c7089e8dd2452e1446aeb93d353cdd64bac761
Nguyen Hoang Son <nghson2812@gmail.com>
2023-05-20 18:16:16 +0700

    update first draft

---
+++ src/resumeData.js
@@ -0,0 +1,84 @@
+let resumeData = {
+    "name": "Nguyen Hoang Son",
+    "role": "Frontend Developer",
+    "linkedinId":"nghson2812",
+    "roleDescription": " Great to welcome you visit my personal website. Please contact me following the below information. Be willing to learn and work in the software field.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/nghson2812/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/nghson2812",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"facebook",
+          "url":"http://facebook.com/nghson2812",
+          "className":"fa fa-facebook"
+        }
+      ],
+    "aboutme":"Second-year CS student. My passion for computers and programming brought me to the College of Engineering and Computer Science at VinUniversity. The most crucial thing I took away from my college was how to tackle problems using inventive thinking and an entrepreneurial attitude. Be willing to learn and work in the software field.",
+    "address":"Vietnam",
+    "website":"",
+    "education":[
+      {
+          "UniversityName":"VinUniversity",
+          "specialization":"Bachelor of Science in Computer Science",
+          "MonthOfPassing":"May",
+          "YearOfPassing":"2025"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"VNPT",
+        "specialization":"5-in-5 Intership",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2022"
+      }
+    ],
+    "skills":[
+      {
+        "skillname":"Programming Languages",
+        "description":"Python, Java, JavaScript, HTML, CSS, SQL."
+      },
+      {
+        "skillname":"Tools/Frameworks",
+        "description":"ReactJS, React Native, Android Studio, Git, LaTeX, Figma, Visual Studio, Web Storm, Eclipse, Matplotlib, NumPy, Pandas."
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"Medinity",
+        "description":"Website",
+        "imgurl":"images/portfolio/medinity-portfolio.png",
+        "url":"https://medinity-website.web.app/"
+      },
+      {
+        "name":"Unifix",
+        "description":"Website",
+        "imgurl":"images/portfolio/unifix-portfolio.png",
+        "url":"https://unifix-48cc3.web.app/login"
+      },
+      {
+        "name":"Kim Ngan interior design",
+        "description":"Website",
+        "imgurl":"images/portfolio/kn-interior-design-portfolio.png",
+        "url":"https://kn-interior-design.web.app/"
+      }
+    ],
+    "contact":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
